249950ae95b795132ba123990c21ecb20342782c
FareehaSiddiqi <96943096+FareehaSiddiqi@users.noreply.github.com>
2024-12-25 21:57:50 +0500

    first commit

---
+++ src/app/(auth)/Course/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Course = () => {
+  return (
+    <div>
+        <h1 className='min-h-screen flex justify-center items-center bg-white text-2xl text-black'>Courses will be available soon...</h1>
+    </div>
+  )
+}
+
+export default Course
